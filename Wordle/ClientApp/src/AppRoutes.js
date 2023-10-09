16482474d2cd05315c7b65d4bfd7f546984064b0
Jonas Wettergrund <jonas.wettergrund@gmail.com>
2023-10-09 19:59:42 +0200

    Added function to fetch game

--- Wordle/ClientApp/src/AppRoutes.js
+++ Wordle/ClientApp/src/AppRoutes.js
@@ -1,10 +1,15 @@
 import ApiAuthorzationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
+import Game from './components/Game';
 import { Home } from "./components/Home";
 
 const AppRoutes = [
   {
     index: true,
     element: <Home />
+  },
+  {
+    path: "/game",
+    element: <Game/>
   },
 
   ...ApiAuthorzationRoutes
