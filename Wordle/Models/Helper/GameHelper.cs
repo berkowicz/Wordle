f28c5615e6e90799ae857bdf616e34cde6675a4f
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-10-17 16:25:13 +0200

    fixed game timer to be correct

--- Wordle/Models/Helper/GameHelper.cs
+++ Wordle/Models/Helper/GameHelper.cs
@@ -65,11 +65,14 @@ namespace Wordle.Models.Helper
                 // Win scenario also Post result to Highscore
                 if (game.GameWord.ToUpper().Equals(guess.ToUpper()))
                 {
+                    TimeSpan timeDifference = DateTime.Now - game.Timer;
+                    int secondsDifference = (int)timeDifference.TotalSeconds;
+
                     game.GameCompleted = true;
                     HighscoreModel hsModel = new HighscoreModel()
                     {
                         Score = game.Score,
-                        Timer = (game.Timer.Second - DateTime.Now.Second),
+                        Timer = secondsDifference,
                         Date = DateTime.Now.Date,
                         GameRefId = game.Id,
                     };
